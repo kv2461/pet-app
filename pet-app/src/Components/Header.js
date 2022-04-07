35dea7794beb695dac31c5cd9b8525cdfd145842
kv2461 <kevinarnoldtv@gmail.com>
2022-04-07 12:02:09 -0700

    initiated app

---
+++ pet-app/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <nav className="navbar-items">
+                <ul>
+                    <li>Hi</li>
+                    <li>Hello</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
