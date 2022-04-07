900f5d37007a09e7e319f0d82e017a4f2e425b55
kv2461 <kevinarnoldtv@gmail.com>
2022-04-07 14:57:53 -0700

    added a mobile-responsive nav bar

--- pet-app/src/App.js
+++ pet-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Components/Header";
+import "./Styles/app.css";
 
 export default function App() {
     return (
--- pet-app/src/Components/Header.js
+++ pet-app/src/Components/Header.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
+import {NavItems} from "./NavItems";
+import "../Styles/navbar.css";
+import {Button} from "./Button";
 
-export default function Header() {
+export default class Header extends Component {
+    state = {clicked:false}
+
+    handleClick = () => {
+        this.setState({clicked:!this.state.clicked});
+    }
+
+    render() {
     return (
         <div>
             <nav className="navbar-items">
-                <ul>
-                    <li>Hi</li>
-                    <li>Hello</li>
+                <h1 className="navbar-logo">Adopt a Pet</h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {NavItems.map((item,index)=>{
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                            </a></li>
+                        )
+                    })}
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         </div>
-    )
+    )}
 }
